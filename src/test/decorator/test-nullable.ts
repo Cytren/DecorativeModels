1b940c586b6cee53ec39db35b3423c781ae7054f
Cytren <contact@cytren.com>
2017-07-27 22:12:14 +0100

    Added required decorator tests

--- src/test/decorator/test-nullable.ts
+++ src/test/decorator/test-nullable.ts
@@ -2,19 +2,38 @@
 import {assert} from "chai";
 import {validate, type, nullable} from "../../main/index";
 
-describe("Model with a nullable property", () => {
-    class NullableModel {
-        @type("any") @nullable property: any;
-    }
+describe("[Nullable Decorator]", () => {
+    describe("Model with a non-nullable property", () => {
+        class NullableModelOne {
+            @type("any") property: any;
+        }
 
-    it("should validate with a null property", () => {
-        let model = new NullableModel();
-        assert.equal(validate(model), true);
+        it("should NOT validate with a null property", () => {
+            let model = new NullableModelOne();
+            assert.equal(validate(model), false);
+        });
+
+        it("should validate with an assigned property", () => {
+            let model = new NullableModelOne();
+            model.property = "STRING";
+            assert.equal(validate(model), true);
+        });
     });
 
-    it("should validate with an assigned property", () => {
-        let model = new NullableModel();
-        model.property = "STRING";
-        assert.equal(validate(model), true);
+    describe("Model with a nullable property", () => {
+        class NullableModelTwo {
+            @type("any") @nullable property: any;
+        }
+
+        it("should validate with a null property", () => {
+            let model = new NullableModelTwo();
+            assert.equal(validate(model), true);
+        });
+
+        it("should validate with an assigned property", () => {
+            let model = new NullableModelTwo();
+            model.property = "STRING";
+            assert.equal(validate(model), true);
+        });
     });
 });
---
+++ src/test/decorator/test-required.ts
@@ -0,0 +1,41 @@
+
+import {assert} from "chai";
+import {model, validate, type, required} from "../../main/index";
+
+describe("[Required Decorator]", () => {
+    describe("Model with a non-required property", () => {
+        @model({ strictMode: false })
+        class RequiredModelOne {
+            @type("any") property: any;
+        }
+
+        it("should validate with a null property", () => {
+            let model = new RequiredModelOne();
+            assert.equal(validate(model), true);
+        });
+
+        it("should validate with an assigned property", () => {
+            let model = new RequiredModelOne();
+            model.property = "STRING";
+            assert.equal(validate(model), true);
+        });
+    });
+
+    describe("Model with a required property", () => {
+        @model({ strictMode: false })
+        class RequiredModelTwo {
+            @type("any") @required property: any;
+        }
+
+        it("should NOT validate with a null property", () => {
+            let model = new RequiredModelTwo();
+            assert.equal(validate(model), false);
+        });
+
+        it("should validate with an assigned property", () => {
+            let model = new RequiredModelTwo();
+            model.property = "STRING";
+            assert.equal(validate(model), true);
+        });
+    });
+});
