26c8089a7e19aa9ff1383b03a7f44920b4f5ff4d
Cytren <contact@cytren.com>
2017-07-27 21:47:55 +0100

    Added the rest of the type tests

--- src/test/type/test-float.ts
+++ src/test/type/test-float.ts
@@ -2,67 +2,67 @@
 import {assert} from "chai";
 import {validate, type} from "../../main/index";
 
-describe("Model with a string property", () => {
-    class StringModel {
-        @type("string") property: any;
+describe("Model with a float property", () => {
+    class FloatModel {
+        @type("float") property: any;
     }
 
     it("should NOT validate with an undefined property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         assert.equal(validate(model), false);
     });
 
-    it("should validate with a string property", () => {
-        let model = new StringModel();
+    it("should NOT validate with a string property", () => {
+        let model = new FloatModel();
         model.property = "STRING";
+
+        assert.equal(validate(model), false);
+    });
+
+    it("should NOT validate with an integer property", () => {
+        let model = new FloatModel();
+        model.property = 123;
 
         assert.equal(validate(model), true);
     });
 
-    it("should NOT validate with an integer property", () => {
-        let model = new StringModel();
-        model.property = 123;
-
-        assert.equal(validate(model), false);
-    });
-
-    it("should NOT validate with an float property", () => {
-        let model = new StringModel();
+    it("should validate with an float property", () => {
+        let model = new FloatModel();
         model.property = 123.45;
 
-        assert.equal(validate(model), false);
+        assert.equal(validate(model), true);
     });
 
     it("should NOT validate with an boolean property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         model.property = true;
 
         assert.equal(validate(model), false);
     });
 
     it("should NOT validate with an array property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         model.property = [];
 
         assert.equal(validate(model), false);
     });
 
     it("should NOT validate with an set property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         model.property = new Set();
 
         assert.equal(validate(model), false);
     });
 
     it("should NOT validate with an map property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         model.property = new Map();
 
         assert.equal(validate(model), false);
     });
 
     it("should NOT validate with an object property", () => {
-        let model = new StringModel();
+        let model = new FloatModel();
         model.property = {};
 
         assert.equal(validate(model), false);
