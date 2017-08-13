ad534f050846527baddb160e38cacc1ec9146be1
Cytren <contact@cytren.com>
2017-08-13 17:15:34 +0100

    Model options optional

--- src/main/model/model.ts
+++ src/main/model/model.ts
@@ -2,8 +2,6 @@
 import {ModelOptions} from "./options";
 import {manager} from "../manager/manager";
 
-export function model(options: ModelOptions): ClassDecorator {
-    return model => {
-        manager.setOptions(model.name, options)
-    };
+export function model(options: ModelOptions = {}): ClassDecorator {
+    return model => manager.setOptions(model.name, options);
 }
